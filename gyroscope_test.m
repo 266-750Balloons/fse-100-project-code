0c48e4a3c1848ee030bf286bef2d5b3244232290
Ben Harper <razorscooter324@gmail.com>
2024-10-24 15:51:32 -0700

    DD GYROSCOPE TEST

---
+++ gyroscope_test.m
@@ -0,0 +1,51 @@
+% I think this source file initially held the test code for motion control, but it 
+% now appears to hold more testing of the ultrasonic sensor.
+% Ben Harper, 2024
+
+larry = brick.UltrasonicDist( 4 );
+total = 0;
+
+while true
+    larry = brick.UltrasonicDist( 4 );
+   
+    while larry > 7
+        disp( larry );
+        brick.MoveMotor('D', -70);
+        
+        brick.MoveMotor('A', -77);
+        larry = brick.UltrasonicDist( 4 );
+        total = total + 1;
+    end
+   
+    if larry <= 7
+        brick.MoveMotor('D', 30);
+        brick.MoveMotor('A', 30);
+        pause(1);
+        
+        % brick.MoveMotorAngleAbs('D', -70, 150, 'Brake');
+        % brick.MoveMotor('D', -70);
+        % brick.MoveMotor('A', -77);
+        brick.GyroCalibrate(3);
+
+        bot_angle = brick.GyroAngle(3);
+        while abs(bot_angle) < 90
+            brick.MoveMotor('D', -70);
+            brick.MoveMotor('A', 77);
+            bot_angle = brick.GyroAngle(3);
+            print( bot_angle );
+        end
+        pause(2);
+
+        brick.StopMotor('D', 'Brake');
+        brick.StopMotor('A', 'Brake');
+        brick.MoveMotor('D', -70);
+        brick.MoveMotor('A', -70);
+        larry = brick.UltrasonicDist( 4 );
+        total = total + 1;
+    end
+    if total >= 500
+        brick.StopMotor('D', 'Brake');
+        brick.StopMotor('A', 'Brake');
+        break;
+    end
+end
