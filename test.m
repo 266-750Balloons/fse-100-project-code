d225e2edbeeebc3b85cca8014d532492e959ee32
Dexx Stexx <drewpad@icloud.com>
2024-10-31 15:49:58 -0700

    Move gyro turn into function

--- test.m
+++ test.m
@@ -1,7 +1,10 @@
 % A test to go straight, left, or right really well with the gyro
 % Ben Harper, Drew Stephens, 2024
 
-% Left is positive, right is negative
+left_speed = -62;
+right_speed = -61.5;
+
+% On gyroscope, left is positive, right is negative
 
 function good = turnLeft( brick_obj )
     bot_angle = brick_obj.GyroAngle(3);
@@ -23,55 +26,41 @@ function good = turnRight( brick_obj )
     good = 1;
 end
 
-left_speed = -62;
-right_speed = -61.5;
+function good = goForwardUntil( min_distance )
+    larry = brick.UltrasonicDist( 4 );
+
+    while larry > min_distance
+            if bot_angle < 0
+                right_speed = right_speed - 0.1;
+            end
+            if bot_angle > 0
+                left_speed = left_speed - 0.1;
+            end
+    
+            disp( larry );
+    
+            brick.MoveMotor('D', left_speed);
+            brick.MoveMotor('A', right_speed);
+    
+            larry = brick.UltrasonicDist( 4 );
+    
+            bot_angle = brick.GyroAngle(3);
+    end
+end
+
 
 
 brick.GyroCalibrate(3);
 bot_angle = brick.GyroAngle(3);
 
-larry = brick.UltrasonicDist( 4 );
-
-while larry > 5
-        if bot_angle < 0
-            right_speed = right_speed - 0.1;
-        end
-        if bot_angle > 0
-            left_speed = left_speed - 0.1;
-        end
-
-        disp( larry );
-
-        brick.MoveMotor('D', left_speed);
-        brick.MoveMotor('A', right_speed);
-
-        larry = brick.UltrasonicDist( 4 );
-
-        bot_angle = brick.GyroAngle(3);
-end
+goForwardUntil( 5 );
 
 % joe = turnLeft(brick);
 bob = turnRight(brick);
 
 larry = brick.UltrasonicDist( 4 );
 
-while larry > 5
-        if bot_angle < 0
-            right_speed = right_speed - 0.1;
-        end
-        if bot_angle > 0
-            left_speed = left_speed - 0.1;
-        end
-
-        disp( larry );
-
-        brick.MoveMotor('D', left_speed);
-        brick.MoveMotor('A', right_speed);
-
-        larry = brick.UltrasonicDist( 4 );
-
-        bot_angle = brick.GyroAngle(3);
-end
+goForwardUntil(5);
 
 brick.StopMotor('D', 'Brake');
 brick.StopMotor('A', 'Brake');
