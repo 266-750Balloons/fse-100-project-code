cbfbab5b2579773a9bc0f580beeece4be8288d28
Ben Harper <razorscooter324@gmail.com>
2024-12-03 15:07:31 -0700

    Real code probably

--- jacktestnew.m
+++ jacktestnew.m
@@ -5,18 +5,36 @@ reset = 0;
 
 while true
     larry = brick.UltrasonicDist(1);
+    pause(0.6)
     touch = brick.TouchPressed(4);
-    larry = brick.UltrasonicDist(1);
-    touch = brick.TouchPressed(4);
+    pause(0.6)
     color = brick.ColorCode(3);
+    pause(0.6)
     disp(larry)
     disp(touch)
+    pause(0.6)
 
     if color == 5
         brick.StopMotor('A', 'Brake');
         brick.StopMotor('D', 'Brake');
-        pause(2);
+        pause(5);
         beep()
+        if (larry >= 40) && (reset == 0)
+            brick.StopMotor('A', 'Brake');
+            brick.StopMotor('D', 'Brake');
+            brick.MoveMotor('D',-40);
+            brick.MoveMotor('A',-40);
+            pause(0.08)
+
+            brick.MoveMotor('D',60);
+            brick.MoveMotor('A',-10);
+            pause(1.1)
+            brick.MoveMotor('D',-40);
+            brick.MoveMotor('A',-40);
+            pause(0.1)
+            reset = 0;
+        end    
+            pause(0.01)
         continue;
     elseif color == stop1
         brick.StopMotor('A', 'Brake');
@@ -27,11 +45,24 @@ while true
         brick.StopMotor('D', 'Brake');
         pause(1);
         break;
+    elseif (larry >= 40) && (reset == 0)
+        brick.StopMotor('A', 'Brake');
+        brick.StopMotor('D', 'Brake');
+        brick.MoveMotor('D',-40);
+        brick.MoveMotor('A',-40);
+        pause(0.08)
 
+        brick.MoveMotor('D',60);
+        brick.MoveMotor('A',-10);
+        pause(1.2)
+        brick.MoveMotor('D',-40);
+        brick.MoveMotor('A',-40);
+        pause(0.3)
+        reset = 0;
     elseif touch == 1
         brick.MoveMotor('D',40);
         brick.MoveMotor('A',40);
-        pause(1)
+        pause(1.5)
 
         brick.MoveMotor('A',60);
         brick.MoveMotor('D',-10);
@@ -40,47 +71,31 @@ while true
         brick.MoveMotor('A',-40);
         pause(0.5)
         reset = 1;
-
-    elseif (larry >= 40) && (reset == 0)
-        brick.StopMotor('A', 'Brake');
-        brick.StopMotor('D', 'Brake');
-        brick.MoveMotor('D',-40);
-        brick.MoveMotor('A',-40);
-        pause(0.8)
-
-        brick.MoveMotor('D',60);
-        brick.MoveMotor('A',-10);
-        pause(1.2)
-        brick.MoveMotor('D',-40);
-        brick.MoveMotor('A',-40);
-        pause(1)
-        pause(0.5)
-        reset = 0;
-
     elseif (larry > 21) && (larry < 40)
-        brick.MoveMotor('D',40);
+        brick.MoveMotor('D',20);
         brick.StopMotor('A','Brake')
-        pause(0.4)
-        brick.MoveMotor('D',-40);
-        brick.MoveMotor('A',-40);
-        pause(0.1)
+        pause(0.03)
+        brick.MoveMotor('D',-30);
+        brick.MoveMotor('A',-30);
+        pause(0.008)
         reset = 0;
-    elseif (larry < 15) || (larry == 255)
+        pause(0.6)
+    elseif (larry < 18) || (larry == 255)
         brick.MoveMotor('A',40);
-        brick.StopMotor('D','Brake')
+        brick.MoveMotor('D',10)
         pause(0.1)
-        brick.MoveMotor('D',-40);
-        brick.MoveMotor('A',-40);
-        pause(0.1)
+        brick.MoveMotor('D',-30);
+        brick.MoveMotor('A',-30);
+        pause(0.01)
         reset = 0;
+        pause(0.6)
     else
         brick.MoveMotor('D',-40);
         brick.MoveMotor('A',-40);
-        pause(0.2)
+        pause(0.03)
         brick.StopMotor('A', 'Brake');
         brick.StopMotor('D', 'Brake');
-        pause(2)
-        larry = brick.UltrasonicDist(1);
+        pause(0.6)
         reset = 0;
     end
 
--- realtest.m
+++ realtest.m
@@ -1,4 +1,5 @@
-while true
+while 1
     touch = brick.TouchPressed(4);
+    pause(0.6)
     disp(touch)
 end
